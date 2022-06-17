b96435606caad70dd42486a8cadd1caa865c385c
austinjp <austin.plunkett@gmail.com>
2022-06-17 11:45:33 +0100

    Added setup.py
    and changed dir name

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+cfg = {
+    "name": 'packaging_with_mypy_b',
+    "version": '0.0.1',
+    "url": 'https://github.com/austinjp/packaging-with-mypy-b',
+    "author": 'austinjp',
+    "packages": ["packaging_with_mypy_b"],
+    "package_data": {
+        "gunicorn":["stubs/gunicorn"],
+    },
+    "install_requires": [
+        'gunicorn',
+    ]
+}
+
+setup(**cfg)
